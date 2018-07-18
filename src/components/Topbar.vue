b789491cc33578426c8dc4c76413ee2171de7d4b
Nolaaaaa <1009198372@qq.com>
2018-07-18 20:48:23 +0800

    添加登录注册按钮并调整样式

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,18 +1,22 @@
 <template>
     <div id="topbar">
-        <div class="logo">VueResumer</div>
+        <div class="logo">Resumer</div>
         <div class="actions">
-            <button>登录</button>
-            <button>注册</button>
+            <el-button  plain>登录</el-button>
+            <el-button type="primary" plain>注册</el-button>
         </div>
     </div>
 </template>
 <style lang="scss"> 
     #topbar{
-        min-height: 100px;
+        min-height: 50px;
         border: 3px solid rgb(243, 171, 171);
         display: flex;  //内容并排显示
-        justify-content: space-between//左右排布
+        justify-content: space-between; //左右排布
+        font-size: 18px;
+        padding: 8px;
+        align-items: center;    //左边x轴上两个居中对齐
     }
+    .el-button{padding: 10px 18px}  //登录注册图标大小
 </style>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import './assets/reset.scss'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
